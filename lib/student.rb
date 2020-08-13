b7c7ce398dbc04413a06a6f23cb5c85561386ad4
Gabriela johnson <johnsongabriela12@gmail.com>
2020-08-13 05:07:51 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,30 +1,29 @@
 class Student
-  
-  attr_accessor :name :grade
+
+  attr_accessor :name, :grade
   attr_reader :id 
-  
-  def initialize (name,grade,id=nil)
-    
-    @name=name
-    @grade=grade
-    @id=id
+
+  def initialize(name, grade, id = nil)
+    @name = name 
+    @grade = grade
+    @id = id 
   end 
-  
-  
+
   def self.create_table
-    sql<<-SQL
-    CREATE TABLE IF NOT EXISTS students(
-    id INTEGER PRIMARY KEY,
-    name TEXT,
-    grade INTEGER,
-    );
-    SQL 
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade TEXT
+      );
+    SQL
+
     DB[:conn].execute(sql)
   end 
-  
+
   def self.drop_table
-    sql<<-SQL
-    DROP TABLE IF EXISTS students;
+    sql = <<-SQL
+      DROP TABLE IF EXISTS students;
     SQL
 
     DB[:conn].execute(sql)
@@ -47,10 +46,5 @@ class Student
     student.save 
     student
   end 
-  
-  
-
-
-  
   
 end
