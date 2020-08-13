6db5642a1183fe0867e8e7a59ae6c35d2eceac61
Gabriela johnson <johnsongabriela12@gmail.com>
2020-08-13 05:01:46 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -17,9 +17,14 @@ class Student
     id INTEGER PRIMARY KEY,
     name TEXT,
     grade INTEGER,
-    )
+    );
     SQL 
     DB[:conn].execute(sql)
+  end 
+  
+  def self.drop_table
+  
+  
   end 
 
   
